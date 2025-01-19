14f7ba27668aa9c823bdd1d388d80c088455e7e9
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-19 23:26:27 +0600

    SimpleForm component create

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,13 @@
+const SimpleForm = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <br/>
+     <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
