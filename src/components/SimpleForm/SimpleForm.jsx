b1bddeb04382db4e05da71f74bd61c74a017111e
Alal Hosen <alalhosen1992620120@gmail.com>
2025-01-19 23:59:43 +0600

    to get name or email - write e props e.target.name.value

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,12 +1,16 @@
 const SimpleForm = () => {
   const handleSubmit = e => {
     e.preventDefault();
+    console.log(e.target.name.value);
+    console.log(e.target.Email.value);
     console.log("form submitted");
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input type="text"name="name" />
+        <br/>
+        <input type="email" name="Email" id="" />
         <br />
         <input type="submit" value="Submit" />
       </form>
