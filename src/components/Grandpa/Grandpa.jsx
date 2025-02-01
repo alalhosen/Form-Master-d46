f04033870d7e0d5d882823327da10512ab9020f9
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-01 23:14:07 +0600

    money & setMoney use with useContext

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,28 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import "./Grandpa.css";
+
 export const AssetContext = createContext("gold");
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
   const asset = "Diamond";
+  const [money, setMoney] =useState(1000);
   return (
     <div className="grandpa">
       <h2>Grandpa</h2>
-      <AssetContext.Provider value="gold">
-        <section className="flex">
-          <Dad asset={asset}></Dad>
-          <Uncle asset={asset}></Uncle>
-          <Aunty></Aunty>
-        </section>
-      </AssetContext.Provider>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <AssetContext.Provider value="gold">
+          <section className="flex">
+            <Dad asset={asset}></Dad>
+            <Uncle asset={asset}></Uncle>
+            <Aunty></Aunty>
+          </section>
+        </AssetContext.Provider>
+      </MoneyContext.Provider>
     </div>
   );
 };
 
 export default Grandpa;
-
 
 /**
  * 1. create a context and export it.
